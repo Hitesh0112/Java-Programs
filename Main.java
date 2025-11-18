ab1efd23e32833fd114c8deb3133863986ac32d4
Hitesh Balaji N <115524164+Hitesh0112@users.noreply.github.com>
2025-11-18 09:50:48 +0530

    Implement FizzBuzz logic

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1=sc.nextInt();
+        if(num1<=0)
+        {
+            System.out.println("Invalid Input Try once more");
+            return;
+        }
+        else
+        {
+            if(num1%3==0&&num1%5==0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(num1%5==0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(num1%3==0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(num1);
+            }
+        }
+    }
+}
